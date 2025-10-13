3481fea5ddb75b14a329b4dcfb3cff5fbd28dbc0
Dedaldino Daniel <dedaldinodev4@gmail.com>
2025-10-13 17:53:06 +0100

    fix: product-details-params

--- src/app/(site)/(pages)/shop/[id]/page.tsx
+++ src/app/(site)/(pages)/shop/[id]/page.tsx
@@ -1,26 +1,22 @@
 "use client"
 
-import { use, useEffect, useState } from "react";
+import React,{ useEffect, useState } from "react";
 import { api } from "@/services";
 import type { Product } from "@/types/product";
 import { customerOnlyProduct } from "@/utils/product";
 import ShopDetailsPage from "@/components/ShopDetails";
 
-interface ShopDetailsProps {
-  params: {
-    id: string;
-  }
-}
 
-export default function ShopDetails({ params }: ShopDetailsProps) {
+
+export default function ShopDetails({ params }) {
 
   const [product, setProduct] = useState<Product | null>(null)
-  const paramId = params.id || 0
+  const { id } = React.use(params)
 
   useEffect(() => {
 
     const getProductData = async () => {
-      const response = await api.get(`products/${paramId}`);
+      const response = await api.get(`products/${id}`);
       const { data } = response.data;
       const result = customerOnlyProduct(data);
       setProduct(result)
