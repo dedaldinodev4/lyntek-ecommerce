13bac34181e8cd4260ccf20e8d760c1033db41ec
Dedaldino Daniel <dedaldinodev4@gmail.com>
2025-10-18 12:42:05 +0100

    feat: contacts-component

--- src/app/(site)/(pages)/contact/page.tsx
+++ src/app/(site)/(pages)/contact/page.tsx
@@ -2,9 +2,7 @@ import Contact from "@/components/Contact";
 
 import { Metadata } from "next";
 export const metadata: Metadata = {
-  title: "Contact Page | NextCommerce Nextjs E-commerce template",
-  description: "This is Contact Page for NextCommerce Template",
-  // other metadata
+  title: "Lyntek | Contactos"
 };
 
 const ContactPage = () => {
